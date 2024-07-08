435c76cf2ce79ad0d25b310c4fa17b0cce5fc1b7
andersonbalves <baratella.anderson@gmail.com>
2024-07-08 14:03:56 -0300

    Detached front and bff to different repositories.

--- back/api/src/form/model/form.model.ts
+++
@@ -1,7 +0,0 @@
-import { FieldModel } from './field.model';
-
-export interface FormModel {
-  path: string;
-  name: string;
-  fields: Array<FieldModel>;
-}
--- front/angular/src/app/components/menu/menu.model.ts
+++
@@ -1,4 +0,0 @@
-export interface MenuModel {
-  label: string;
-  action: string | Array<MenuModel>;
-}
